e64948766fc89a0feb89f76f41c2f644555a29c9
ManliestBen <benjamintmanley@gmail.com>
2020-12-09 17:13:42 -0600

    add user show page

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ git reset --hard origin/main
 
 # MEN Stack with OAuth Template
 
-4. Add comment UI
-5. Add comment POST route/controller
 6.  User index view
 7.  User show view
 8.  My Pets view
--- controllers/users.js
+++ controllers/users.js
@@ -2,5 +2,23 @@ const Pet = require('../models/pet')
 const User = require('../models/user')
 
 module.exports = {
-  
+  index,
+  show
+}
+
+function index(req, res) {
+  User.find({})
+  .then((users) => {
+    res.render('users/index', {users, user: req.user})
+  })
+}
+
+function show(req, res) {
+  User.findById(req.params.id)
+  .then((user) => {
+    Pet.find({ owner: user._id })
+    .then((pets) => {
+      res.render('users/show', {user: req.user, user, pets})
+    })
+  })
 }
