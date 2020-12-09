fe16ac66a04f2db3a0ebc4946c817d7db22cbc7a
ManliestBen <benjamintmanley@gmail.com>
2020-12-09 16:02:30 -0600

    add index functionality

--- controllers/pets.js
+++ controllers/pets.js
@@ -2,9 +2,27 @@ const Pet = require('../models/pet')
 const User = require('../models/user')
 
 module.exports = {
-  new: newPet
+  new: newPet,
+  create,
+  index
 }
 
 function newPet(req, res) {
   res.render('pets/new', {user: req.user})
+}
+
+function create(req, res) {
+  req.body.owner = req.user._id
+  Pet.create(req.body)
+  .then(() => {
+    res.redirect('/pets')
+  })
+}
+
+function index(req, res) {
+  Pet.find({})
+  .populate('owner')
+  .then((pets) => {
+    res.render('pets/index', {pets, user: req.user})
+  })
 }
--- routes/pets.js
+++ routes/pets.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const petsCtrl = require('../controllers/pets')
 
 router.get('/new', isLoggedIn, petsCtrl.new)
+router.post('/', isLoggedIn, petsCtrl.create)
+router.get('/', isLoggedIn, petsCtrl.index)
 
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) return next()
